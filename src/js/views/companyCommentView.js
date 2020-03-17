e6bad8d839ce412b4a3ff50d22707e8fb6e29fee
ygzaydn <ygzaydns@gmail.com>
2020-03-17 09:48:36 +0300

    companycommentmodel & companycommentview is modalized

--- src/js/views/companyCommentView.js
+++ src/js/views/companyCommentView.js
@@ -1,28 +1,6 @@
-
-const companyComment = document.querySelectorAll('.form-control');
-const companyCommentArr = Array.from(companyComment);
-
-const submit = document.getElementById('post_comment');
-const commentList = document.querySelector('.comment-list');
-const commentInput = document.getElementById('message');
-
-
-class new_comment {
-
-    constructor(name, email, message, avatar){
-        this.name = name;
-        this.email = email;
-        this.message = message;
-        this.avatar = avatar;
-    }
-
-    summarize = () => {
-        console.log(`Name : ${this.name}
-        Email : ${this.email}
-        Message : ${this.message}`)
-    }
-}
-
+import('../models/companyCommentModel.js')
+.then ((module) => 
+{
 getTime = () => {
   let date = new Date();
 
@@ -32,7 +10,7 @@ getTime = () => {
 
 template = (data) => {
 
-  commentList.insertAdjacentHTML("afterbegin",`
+  module.commentList.insertAdjacentHTML("afterbegin",`
   <li class="comment">
     <div class="vcard bio">
       <img src="${data.avatar}" alt="Image placeholder">
@@ -49,14 +27,14 @@ template = (data) => {
 document.getElementById("post_comment").addEventListener("click", (event)=>{
 
     const newComment = {
-        name : companyCommentArr[0],
-        email : companyCommentArr[1],
-        message : companyCommentArr[2],
-        avatar : "images/comment_vcard.jpg",
+        name : module.companyCommentArr[0],
+        email : module.companyCommentArr[1],
+        message : module.companyCommentArr[2],
+        avatar : "/src/images/comment_vcard.jpg"
 
     }
 
-    const comment = new new_comment(newComment.name.value, newComment.email.value, newComment.message.value, newComment.avatar);
+    const comment = new module.new_comment(newComment.name.value, newComment.email.value, newComment.message.value, newComment.avatar);
 
     comment.summarize();
     event.preventDefault();
@@ -64,13 +42,11 @@ document.getElementById("post_comment").addEventListener("click", (event)=>{
     template(comment);
 
     // Save the list to localStorage
-    localStorage.setItem('commentListing', commentList.innerHTML);
+    localStorage.setItem('commentListing', module.commentList.innerHTML);
     window.open(`${window.location.href}`);
     } else {
       alert (`Bilgilerinizi kontrol edin`);
     }
-    //let commentPlace = document.querySelector('.comment-list');
-    //commentPlace.insertAdjacentHTML('beforeend','<li class="comment"><div class="vcard bio"><img src="images/comment_vcard.jpg" alt="Image placeholder"></div><div class="comment-body"><h3>User 3</h3><div class="meta">January 9, 2018 at 2:21pm</div><p>Yorum 3</p></div></li>');
 })
 
 
@@ -79,5 +55,7 @@ const saved = localStorage.getItem('commentListing');
 
 // Check if it exists and if so set HTML to value
 if (saved) {
-  commentList.innerHTML = saved;
+  module.commentList.innerHTML = saved;
 }
+
+})
